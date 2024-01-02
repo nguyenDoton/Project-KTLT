8f8da27b4a56ee731791adc150e773276f01cbba
nguyenDoton <76211824+nguyenDoton@users.noreply.github.com>
2024-01-02 16:09:32 +0700

    Add files via upload

--- home.cpp
+++ home.cpp
@@ -3,10 +3,30 @@
 #include <iomanip>
 #include <cstdlib>
 #include <time.h>
+#include <conio.h>
 
 int score = 0;
 bool playable = true;
 using namespace std;
+
+class Playground{
+    int size;
+    public:
+    Playground(){
+       size =0;
+    }
+    void createPlayground(int i){
+        if(i >=3){
+            size = i;
+            int Playground[size][size] ;
+        }else{
+            cout <<"Size not allowed"<<endl;
+        }
+    }
+
+
+    
+};
 
 void generate(int arr[4][4]){
     srand(time(0));
@@ -106,24 +126,28 @@ void movedown(int arr[4][4]){
     }
    }
 }
-// int check(int arr[4][4]){
-//     for(int i =0;i<3;i++){
-//         for(int j =0;j<3;j++){
-//             if((arr[i][j] != arr[i+1][j] && arr[i+1][j]!=0) 
-//             && (arr[i][j] != arr[i][j+1] && arr[i][j+1] != 0)
-//             && (arr[i-1][j] != arr[i][j] && arr[i-1][j]!=0) 
-//             && (arr[i][j-1] != arr[i][j]&& arr[i][j-1])!=0){ 
-//                     cout << arr[i][j]<<endl;
-//                     cout << "You lost!" << endl;
-//                     playable = false;
-//                     return 0;
-//             }
-//             else{
-//                 return 1;
-//             }
-//         }
-//     }
-// }
+int check(int arr[4][4]){
+    for(int i =0;i<3;i++){
+        for(int j =0;j<3;j++){
+            // if((arr[i][j] != arr[i+1][j] && arr[i+1][j]!=0) 
+            // && (arr[i][j] != arr[i][j+1] && arr[i][j+1] != 0)
+            // && (arr[i-1][j] != arr[i][j] && arr[i-1][j]!=0) 
+            // && (arr[i][j-1] != arr[i][j]&& arr[i][j-1])!=0){ 
+            //         cout << arr[i][j]<<endl;
+            //         cout << "You lost!" << endl;
+            //         playable = false;
+            //         return 0;
+            // }
+            // else{
+            //     return 1;
+            // }
+            if(arr[i][j] ==2048){
+                cout<<"You won!"<<endl;
+                playable = false;
+            }
+        }
+    }
+}
 void print(int arr[4][4]){
       for(int i =0;i<4;i++){
         for(int j =0;j<4;j++){
@@ -131,6 +155,14 @@ void print(int arr[4][4]){
         }
         cout<<"\n"<<"\n";
     }
+}
+void Restart(int arr[4][4]){
+    memset(arr,0,sizeof(arr));
+    // playground[0][0] =4;
+    // playground[0][1] =4;
+    arr[0][3] =2;
+    score = 0;
+
 }
 void play(int arr[4][4]){
     char x;
@@ -147,18 +179,24 @@ void play(int arr[4][4]){
     else if(x =='d'){
         movedown(arr);
     }
-    else if(x == 'E'){
+    else if(x == 'S'){
         playable = false;
+        cout<<
+        "Game stopped"<<endl;
+    }
+    else if(x == 'R'){
+        Restart(arr);
     }
     else{
         cout<<"Wrong letter,try again"<<endl;
     }
 }
+
 int main(){
     int playground[4][4];char ans;
     memset(playground,0,sizeof(playground));
-    playground[0][0] =4;
-    playground[0][1] =4;
+    // playground[0][0] =4;
+    // playground[0][1] =4;
     playground[0][3] =2;
     cout<<"Ready to play?Type c if yes or any letter if no(lowercase)"<<endl;
     cin >> ans;
@@ -167,10 +205,15 @@ int main(){
            check(playground);
            print(playground);
            cout << "Score:"<<score<<endl;
-           cout << "Type l to move left,r to move right,u to move up and d to move down"<<endl;
-           cout <<"Type E to stop the game"<<endl;
+           cout << "Type l to move left"
+           <<endl<<"     r to move right"
+           <<endl<<"     u to move up "
+           <<endl<<"     d to move down"
+           <<endl<<"     R to restart the game"
+           <<endl;
+           cout <<"Type S to stop the game"<<endl;
            play(playground);
-           
+           system("cls");
         }
     }else{
         return 0;
